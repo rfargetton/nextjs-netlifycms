b9370e2edf18c05ba49f23af91002e9ead25e3c5
rfargetton <romaric.fargetton@gmail.com>
2024-02-26 22:26:05 +0100

    Tailwind update / cleanup

--- src/components/image-placeholder/index.js
+++ src/components/image-placeholder/index.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { BlurhashCanvas } from "react-blurhash";
 
 export default function ImageWithPlaceholder({ imageProps }) {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       lineHeight: {
